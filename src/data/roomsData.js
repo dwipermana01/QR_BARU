5a8c0dffbe73e6d6beb3eceeb4fdd8b6b7c6d7db
Dwi_Permana <dwipermana01@gmail.com>
2025-09-24 08:28:17 +0700

    update VII A

--- src/data/roomsData.js
+++ src/data/roomsData.js
@@ -283,7 +283,7 @@ export const roomsData = {
       { icon: "/icons/frame.svg", name: "Bingkai", count: "2 Unit" },
       { icon: "/icons/hangingWhiteboard.svg", name: "Papan Data Informasi", count: "1 Unit" }
     ],
-    mapImage: "",
+    mapImage: "/maps/r030106.jpg",
     legendItems: [
       { number: "", name: "", color: "" }
     ]
@@ -937,27 +937,8 @@ export const roomsData = {
       }
     ],
     assets: [
-      { icon: "/icons/table.svg", name: "Meja Siswa", count: "30 unit" },
-      { icon: "/icons/studentChair.svg", name: "Kursi Siswa", count: "30 unit" },
-      { icon: "/icons/teacherDesk.svg", name: "Meja Guru", count: "1 unit" },
-      { icon: "/icons/teacherChair.svg", name: "Kursi Guru", count: "1 unit" },
-      { icon: "/icons/speaker.svg", name: "Speaker", count: "2 unit" },
-      { icon: "/icons/airConditioner.svg", name: "Air Conditioner", count: "2 unit" },
-      { icon: "/icons/projector.svg", name: "Proyektor", count: "1 unit" },
-      { icon: "/icons/projectorScreen.svg", name: "Screen Proyektor", count: "1 unit" },
-      { icon: "/icons/whiteboardFrame.svg", name: "Papan Tulis", count: "2 Unit" },
-      { icon: "/icons/cctv.svg", name: "CCTV", count: "1 Unit" },
-      { icon: "/icons/ipCamera.svg", name: "IP Camera", count: "1 Unit" },
-      { icon: "/icons/lockers.svg", name: "Loker", count: "2 Unit" },
-      { icon: "/icons/clock.svg", name: "Jam Dinding", count: "1 Unit" },
-      { icon: "/icons/trash.svg", name: "Tempat Sampah", count: "1 Unit" },
-      { icon: "/icons/cabinet.svg", name: "Lemari", count: "2 Unit" },
-      { icon: "/icons/curtain.svg", name: "Gorden", count: "7 Unit" },
-      { icon: "/icons/cross.svg", name: "Salib", count: "1 Unit" },
-      { icon: "/icons/flag.svg", name: "Bendera", count: "1 Set" },
-      { icon: "/icons/garudaPancasila.svg", name: "Lambang Garuda", count: "1 Unit" },
-      { icon: "/icons/frame.svg", name: "Bingkai", count: "2 Unit" },
-      { icon: "/icons/hangingWhiteboard.svg", name: "Papan Data Informasi", count: "1 Unit" }
+      { icon: "/icons/table.svg", name: "Meja", count: "2 unit" },
+      { icon: "/icons/apar.svg", name: "Alat Pemadam Api", count: "1 Unit" }
     ],
     mapImage: "",
     legendItems: [
